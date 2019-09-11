4dc0c454b7689c5ca29dd279022611c45941d79c
Michael Morris <mmorris1990@gmail.com>
2019-09-11 14:39:17 -0500

    created components for app. Added scorecard functionality and game over alert

--- src/App.js
+++ src/App.js
@@ -1,130 +1,64 @@
-import React, { Component } from 'react';
-import GridMDC from "./components/GridMDC";
-import PaperMDC from "./components/PaperMDC";
-import CharCard from "./components/CharCard";
-import Score from "./components/Score";
-import Alert from "./components/Alert";
-import NavBar from "./components/NavBar";
-import BottomNavMDC from "./components/BottomNavMDC";
-import characters from "./characters.json";
+import React, { Component } from "react";
+import Card from "./components/Card/Card";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Header from "./components/Header/Header";
+import cards from "./cards.json";
+import "./App.css";
 
 class App extends Component {
-
+  // Setting this.state.cards to the cards json array
   state = {
-    characters: characters,
-    pickedChars: [],
-    topScore: 0,
-    alertMessage: ""
-  }
+    cards,
+    score: 0,
+    highscore: 0
+  };
 
-  handlePicked = event => {
-
-    const name = event.target.attributes.getNamedItem("name").value;
-    this.shuffleCharacters()
-    this.checkGuess(name, this.updateTopScore)
-  }
-
-  shuffleCharacters = () => {
-    this.setState(this.state.characters = this.shuffleArray(this.state.characters))
-  }
-
-  shuffleArray = (a) => {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
     }
-    return a;
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
   }
 
-  checkGuess = (name, cb) => {
-    const newState = { ...this.state };
-    if (newState.pickedChars.includes(name)) {
-      newState.alertMessage = `YOU ALREADY PICKED "${name.toUpperCase()}"!`
-      newState.pickedChars = []
-      this.setState(this.state = newState)
-    } else {
-      newState.pickedChars.push(name)
-      newState.alertMessage = `GOOD CHOICE!`
-      this.setState(this.state = newState)
-    }
-    cb(newState, this.alertWinner)
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
   }
-
-  updateTopScore = (newState, cb) => {
-    if (newState.pickedChars.length > newState.topScore) {
-      newState.topScore++
-      this.setState(this.state = newState)
-    }
-    cb(newState)
-  }
-
-  alertWinner = (newState) => {
-    if (newState.pickedChars.length === 12) {
-      newState.alertMessage = "CHAMPION!";
-      newState.pickedChars = [];
-      this.setState(this.state = newState)
-    }
-  }
-
+  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
-      <div>
-        <NavBar style={{ background: "#313133", marginBottom: "5px" }} />
-
-        <GridMDC container direction="column" style={{ margin: "0 auto", maxWidth: 945 }}>
-
-          <GridMDC item lg={12}>
-            <PaperMDC>
-              {this.state.alertMessage === "GOOD CHOICE!" ? (
-                <Alert message={this.state.alertMessage} style={{ color: "green" }} />
-              ) : (
-                  <Alert message={this.state.alertMessage} style={{ color: "red" }} />
-                )}
-            </PaperMDC>
-          </GridMDC>
-
-          <GridMDC container justify="space-between">
-
-            <GridMDC item lg={6} md={6} sm={12} xs={12}>
-              <PaperMDC>
-                <Score type="Score" score={this.state.pickedChars.length} />
-              </PaperMDC>
-            </GridMDC>
-
-            <GridMDC item lg={6} md={6} sm={12} xs={12}>
-              <PaperMDC>
-                <Score type="Top Score" score={this.state.topScore} />
-              </PaperMDC>
-            </GridMDC>
-
-          </GridMDC>
-
-        </GridMDC>
-
-        <GridMDC container spacing={24} justify="center" style={{ maxWidth: 945, margin: "0 auto" }}>
-          {this.state.characters.map(char => (
-            <GridMDC item lg={3} md={3} sm={4} xs={6}>
-            <CharCard
-              id={char.id}
-              name={char.name}
-              image={char.image}
-              key={char.id}
-              handlePicked={this.handlePicked}
-            />
-            </GridMDC>
-          ))}
-        </GridMDC>
-        <BottomNavMDC style={{ background: "#313133", marginTop: "17.5px", paddingTop: "15px", borderTop: "2.5px solid slategray" }}>
-          <a href="https://github.com/mmorris1990/React-clickyGame" target="_blank" className="link" alt="clicky-game-github-link"><i className="fa fa-github fa-2x"></i></a>
-        </BottomNavMDC>
-
-      </div>
-    )
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
   }
 }
 
 export default App;
-
---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+  return (
+    <div className="card" onClick={() => props.clickCount(props.id)}>
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+    </div>
+  )
+
+};
+
+export default Card;
